f33caf8f653d0e719e30f2bb13bd256d1db75d69
Bianncajs <biancajesus.silva@outlook.com>
2025-01-16 16:14:52 -0300

    Implementação rotas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,31 +1,35 @@
-function Navbar() {
-    return (
-        <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-            <div className="container flex justify-between items-center text-lg">
-                <div className="font-semibold text-xl">
-                    Blog Pessoal
-                </div>
+import { Link } from "react-router-dom";
 
-                <div className="flex gap-6">
-                    <button className="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-400 transition duration-300">
-                        Postagens
-                    </button>
-                    <button className="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-400 transition duration-300">
-                        Temas
-                    </button>
-                    <button className="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-400 transition duration-300">
-                        Cadastrar tema
-                    </button>
-                    <button className="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-400 transition duration-300">
-                        Perfil
-                    </button>
-                    <button className="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-400 transition duration-300">
-                        Sair
-                    </button>
-                </div>
-            </div>
+function Navbar() {
+  return (
+    <div className="w-full bg-[#5C7B91] text-white flex justify-center py-4">
+      <div className="container flex justify-between items-center text-lg">
+        <div className="font-semibold text-xl">
+          <Link to="/home" className="text-2xl font-bold">
+            Blog Pessoal
+          </Link>
         </div>
-    )
+
+        <div className="flex gap-6">
+          <button className="px-4 py-2 bg-[#3A5A77] hover:bg-[#2E4757] rounded-md focus:outline-none focus:ring-2 focus:ring-[#3A5A77] transition duration-300">
+            Postagens
+          </button>
+          <button className="px-4 py-2 bg-[#3A5A77] hover:bg-[#2E4757] rounded-md focus:outline-none focus:ring-2 focus:ring-[#3A5A77] transition duration-300">
+            Temas
+          </button>
+          <button className="px-4 py-2 bg-[#3A5A77] hover:bg-[#2E4757] rounded-md focus:outline-none focus:ring-2 focus:ring-[#3A5A77] transition duration-300">
+            Cadastrar tema
+          </button>
+          <button className="px-4 py-2 bg-[#3A5A77] hover:bg-[#2E4757] rounded-md focus:outline-none focus:ring-2 focus:ring-[#3A5A77] transition duration-300">
+            Perfil
+          </button>
+          <button className="px-4 py-2 bg-[#3A5A77] hover:bg-[#2E4757] rounded-md focus:outline-none focus:ring-2 focus:ring-[#3A5A77] transition duration-300">
+            Sair
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,14 @@
 function Home() {
     return (
         <>
-            <div className="bg-indigo-900 flex justify-center">
+            <div className="bg-[#2C3E50] flex justify-center">
                 <div className='container grid grid-cols-2 text-white'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
-                            Seja Bem Vinde!
+                            Seja Bem-Vindo(a)!
                         </h2>
                         <p className='text-xl'>
-                            Expresse aqui seus pensamentos e opniões
+                            Expresse aqui seus pensamentos e opiniões 😊
                         </p>
 
                         <div className="flex justify-around gap-4">
@@ -22,7 +22,7 @@ function Home() {
 
                     <div className="flex justify-center ">
                         <img
-                            src="https://i.imgur.com/fyfri1v.png"
+                            src="https://ik.imagekit.io/yjt8pt6d8/8392859.png?updatedAt=1737041250767"
                             alt="Imagem Página Home"
                             className='w-2/3'
                         />
