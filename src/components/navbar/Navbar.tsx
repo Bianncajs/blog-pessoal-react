943434a36d72c12eacf3f752fe16bb4556494186
Bianncajs <biancajesus.silva@outlook.com>
2025-01-26 22:14:24 -0300

    Ajustes finais

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,20 +1,26 @@
-import { useContext } from "react";
+import { ReactNode, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
+import { ToastAlerta } from "../../utils/ToastAlerta";
 
 function Navbar() {
-  const navigate = useNavigate();
 
-    const { handleLogout } = useContext(AuthContext)
+    const navigate = useNavigate();
+
+    const { usuario, handleLogout } = useContext(AuthContext)
 
     function logout() {
 
         handleLogout()
-        alert('O Usuário foi desconectado com sucesso!')
+        ToastAlerta('O Usuário foi desconectado com sucesso!', 'info')
         navigate('/')
     }
+    
+    let component: ReactNode
 
-  return (
+    if (usuario.token !== "") {
+
+        component = (
     <div className="w-full bg-[#5C7B91] text-white flex justify-center py-4">
       <div className="container flex justify-between items-center text-lg">
         <div className="font-semibold text-xl">
@@ -34,15 +40,21 @@ function Navbar() {
           <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
           </button>
           <button className="px-4 py-2 bg-[#3A5A77] hover:bg-[#2E4757] rounded-md focus:outline-none focus:ring-2 focus:ring-[#3A5A77] transition duration-300">
-            Perfil
+          <Link to='/perfil' className='hover:underline'>Perfil</Link>
           </button>
           <button className="px-4 py-2 bg-[#3A5A77] hover:bg-[#2E4757] rounded-md focus:outline-none focus:ring-2 focus:ring-[#3A5A77] transition duration-300">
-            Sair
+          <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
           </button>
         </div>
       </div>
     </div>
   );
+}
+return (
+  <>
+      { component }
+  </>
+)
 }
 
 export default Navbar;
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,59 +1,60 @@
-import { createContext, ReactNode, useState } from "react";
+import { ReactNode, createContext, useState } from "react";
 import UsuarioLogin from "../models/UsuarioLogin";
 import { login } from "../services/Service";
+import { ToastAlerta } from "../utils/ToastAlerta";
 
 interface AuthContextProps {
-  usuario: UsuarioLogin;
-  handleLogout(): void;
-  handleLogin(usuario: UsuarioLogin): Promise<void>;
-  isLoading: boolean;
+    usuario: UsuarioLogin
+    handleLogout(): void
+    handleLogin(usuario: UsuarioLogin): Promise<void>
+    isLoading: boolean
 }
 
-interface AuthProviderProps {
-  children: ReactNode;
+interface AuthProvidersProps {
+    children: ReactNode
 }
 
-export const AuthContext = createContext({} as AuthContextProps);
+export const AuthContext = createContext({} as AuthContextProps)
 
-export function AuthProvider({ children }: AuthProviderProps) {
-  const [usuario, setUsuario] = useState<UsuarioLogin>({
-    id: 0,
-    nome: "",
-    usuario: "",
-    senha: "",
-    foto: "",
-    token: "",
-  });
+export function AuthProvider({ children }: AuthProvidersProps) {
 
-  const [isLoading, setIsLoading] = useState(false);
+    const [usuario, setUsuario] = useState<UsuarioLogin>({
+        id: 0,
+        nome: '',
+        usuario: '',
+        senha: '',
+        foto: '',
+        token: ''
+    })
 
-  async function handleLogin(usuarioLogin: UsuarioLogin) {
-    setIsLoading(true);
-    try {
-      await login(`/usuarios/logar`, usuarioLogin, setUsuario);
-      alert("O Usuário foi autenticado com sucesso!");
-    } catch (error) {
-      alert("Os Dados do usuário estão inconsistentes!");
+    const [isLoading, setIsLoading] = useState(false)
+
+    async function handleLogin(usuarioLogin: UsuarioLogin) {
+        setIsLoading(true)
+
+        try {
+            await login(`/usuarios/logar`, usuarioLogin, setUsuario)
+            ToastAlerta("O Usuário foi autenticado com sucesso!", "sucesso")
+        } catch (error) {
+            ToastAlerta("Os dados do Usuário estão inconsistentes!", "erro")
+        }
+        setIsLoading(false)
     }
-    setIsLoading(false);
-  }
 
-  function handleLogout() {
-    setUsuario({
-      id: 0,
-      nome: "",
-      usuario: "",
-      senha: "",
-      foto: "",
-      token: "",
-    });
-  }
+    function handleLogout() {
+        setUsuario({
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: '',
+            foto: '',
+            token: ''
+        })
+    }
 
-  return (
-    <AuthContext.Provider
-      value={{ usuario, handleLogin, handleLogout, isLoading }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+    return(
+        <AuthContext.Provider value={{ usuario, handleLogin, handleLogout, isLoading}}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
